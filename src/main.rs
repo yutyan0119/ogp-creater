6e555322c68f36db07a1365312a83f6e86846b2a
Yuto Nakamura <maron07222010@gmail.com>
2023-06-04 00:03:58 +0900

    形態要素解析を使用するようにした

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use image::{ImageBuffer, Rgba};
 use imageproc::drawing::draw_filled_circle_mut;
 use imageproc::drawing::draw_filled_rect_mut;
 use imageproc::drawing::draw_text_mut;
+use lindera_core::{mode::Mode, LinderaResult};
+use lindera_dictionary::{DictionaryConfig, DictionaryKind, UserDictionaryConfig};
+use lindera_tokenizer::tokenizer::{Tokenizer, TokenizerConfig};
 use rusttype::{Font, Scale};
 use std::env;
+use std::path::PathBuf;
 
 fn create_background(width: u32, height: u32, color: Rgba<u8>) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
     ImageBuffer::from_fn(width, height, |_, _| color)
@@ -94,134 +98,108 @@ fn draw_icon(image: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, icon_path: &str) {
     );
 }
 
-fn draw_centered_text(
+fn split_string(
+    text: &str,
+    target_width: f32,
+    font: &Font,
+    default_scale: Scale,
+) -> LinderaResult<Vec<String>> {
+    let dictionary: DictionaryConfig = DictionaryConfig {
+        kind: Some(DictionaryKind::IPADIC),
+        path: None,
+    };
+
+    let user_dictionary = Some(UserDictionaryConfig {
+        kind: Some(DictionaryKind::IPADIC),
+        path: PathBuf::from("/home/yuto/ogp-creater/user-dict.csv"),
+    });
+
+    let config: TokenizerConfig = TokenizerConfig {
+        dictionary,
+        user_dictionary,
+        mode: Mode::Normal,
+    };
+
+    let tokenizer: Tokenizer = Tokenizer::from_config(config)?;
+
+    let tokens: Vec<lindera_tokenizer::token::Token> = tokenizer.tokenize(text)?;
+    let mut lines = Vec::new();
+    let mut current_line = String::new();
+    let mut current_width = 0.0;
+
+    for token in tokens {
+        let word = token.text;
+        let word_width: f32 = word
+            .chars()
+            .map(|c| {
+                font.glyph(c)
+                    .scaled(default_scale)
+                    .h_metrics()
+                    .advance_width
+            })
+            .sum::<f32>();
+
+        if current_width + word_width <= target_width {
+            current_line.push_str(word);
+            current_width += word_width;
+        } else {
+            lines.push(current_line);
+            current_line = word.to_string();
+            current_width = word_width;
+        }
+    }
+
+    lines.push(current_line); // Add the last line
+    Ok(lines)
+}
+
+fn draw_centered_lines(
+    image: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
+    font: &Font,
+    lines: &[String],
+    color: Rgba<u8>,
+    scale: Scale,
+) {
+    let v_metrics: rusttype::VMetrics = font.v_metrics(scale);
+    let line_height: f32 = v_metrics.ascent - v_metrics.descent + v_metrics.line_gap;
+    let text_height: f32 = line_height * lines.len() as f32;
+    let start_y: f32 = (image.height() as f32 - text_height) / 2.0;
+
+    for (i, line) in lines.iter().enumerate() {
+        let text_width: f32 = line
+            .chars()
+            .map(|c| font.glyph(c).scaled(scale).h_metrics().advance_width)
+            .sum::<f32>();
+
+        let start_x: f32 = (image.width() as f32 - text_width) / 2.0;
+        let y_position: f32 = start_y + line_height * i as f32;
+
+        draw_text_mut(
+            image,
+            color,
+            start_x as i32,
+            y_position as i32,
+            scale,
+            &font,
+            line,
+        );
+    }
+}
+
+fn draw_text_on_image(
     image: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
     font: &Font,
     text: &str,
     color: Rgba<u8>,
-) {
+) -> LinderaResult<()> {
     let target_width: f32 = 630.0;
-    let default_scale: Scale = Scale::uniform(50.0);
-    let text_width_default_scale: f32 = text
-        .chars()
-        .map(|c| {
-            font.glyph(c)
-                .scaled(default_scale)
-                .h_metrics()
-                .advance_width
-        })
-        .sum::<f32>();
-    let scale_factor: f32 = target_width / text_width_default_scale;
-    let scale: Scale = Scale::uniform(50.0 * scale_factor);
-    let max_scale = 70.0;
-    let v_metrics: rusttype::VMetrics = font.v_metrics(scale);
-    let text_height: f32 = v_metrics.ascent - v_metrics.descent + v_metrics.line_gap;
-    let start_x: f32 = 285.0;
-    let start_y: f32 = (image.height() as f32 - text_height) / 2.0 + 35.0;
-    if max_scale < scale.x {
-        if scale.x > 100 as f32 {
-            let text_width_scale: f32 = text
-                .chars()
-                .map(|c| {
-                    font.glyph(c)
-                        .scaled(Scale::uniform(100.0))
-                        .h_metrics()
-                        .advance_width
-                })
-                .sum::<f32>();
-            let v_metrics: rusttype::VMetrics = font.v_metrics(Scale::uniform(100.0));
-            let text_height: f32 = v_metrics.ascent - v_metrics.descent + v_metrics.line_gap;
-            let start_x = (image.width() as f32 - text_width_scale) / 2.0;
-            let start_y = (image.height() as f32 - text_height) / 2.0 + 35.0;
-            draw_text_mut(
-                image,
-                color,
-                start_x as i32,
-                start_y as i32,
-                Scale::uniform(100.0),
-                &font,
-                text,
-            );
-        } else {
-            draw_text_mut(
-                image,
-                color,
-                start_x as i32,
-                start_y as i32,
-                scale,
-                &font,
-                text,
-            );
-        }
-    } else {
-        let split_index: usize = text.chars().count() / 2;
-        let (first_half, second_half) = text.chars().enumerate().fold(
-            (String::new(), String::new()),
-            |(mut first, mut second), (i, c)| {
-                if i < split_index {
-                    first.push(c);
-                } else {
-                    second.push(c);
-                }
-                (first, second)
-            },
-        );
+    let scale: Scale = Scale::uniform(80.0);
 
-        let first_half_width_default_scale: f32 = first_half
-            .chars()
-            .map(|c| {
-                font.glyph(c)
-                    .scaled(default_scale)
-                    .h_metrics()
-                    .advance_width
-            })
-            .sum::<f32>();
-        let second_half_width_default_scale = second_half
-            .chars()
-            .map(|c| {
-                font.glyph(c)
-                    .scaled(default_scale)
-                    .h_metrics()
-                    .advance_width
-            })
-            .sum::<f32>();
-        let max_half_width: f32 =
-            first_half_width_default_scale.max(second_half_width_default_scale);
-        let scale_factor: f32 = target_width / max_half_width;
-        let new_scale: Scale = Scale::uniform(50.0 * scale_factor);
+    let lines: Vec<String> = split_string(text, target_width, font, scale)?;
 
-        let first_half_width: f32 = first_half
-            .chars()
-            .map(|c| font.glyph(c).scaled(new_scale).h_metrics().advance_width)
-            .sum::<f32>();
-        let second_half_width = second_half
-            .chars()
-            .map(|c| font.glyph(c).scaled(new_scale).h_metrics().advance_width)
-            .sum::<f32>();
-        let start_x_first: f32 = (image.width() as f32 - first_half_width) / 2.0;
-        let start_x_second: f32 = (image.width() as f32 - second_half_width) / 2.0;
-        let start_y_first: f32 = image.height() as f32 / 2.0 - text_height * scale_factor - 10.0;
-        let start_y_second: f32 = image.height() as f32 / 2.0;
+    draw_centered_lines(image, font, &lines, color, scale);
 
-        draw_text_mut(
-            image,
-            color,
-            start_x_first as i32,
-            start_y_first as i32,
-            new_scale,
-            &font,
-            &first_half,
-        );
-        draw_text_mut(
-            image,
-            color,
-            start_x_second as i32,
-            start_y_second as i32,
-            new_scale,
-            &font,
-            &second_half,
-        );
-    }
+    Ok(())
 }
 
 fn draw_caption(image: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, font: &Font, text: &str) {
@@ -248,7 +226,8 @@ fn create_ogp_image(text: &str, icon_path: &str, font: &Font) -> ImageBuffer<Rgb
     draw_rounded_white_rect(&mut image);
     draw_title_text(&mut image, font, "ゆーちゃんのブログ");
     draw_icon(&mut image, icon_path);
-    draw_centered_text(&mut image, font, text, maincolor);
+    let _ = draw_text_on_image(&mut image, font, text, maincolor);
+    // draw_centered_text(&mut image, font, text, maincolor);
     draw_caption(&mut image, font, "@yutyan_ut");
 
     image
